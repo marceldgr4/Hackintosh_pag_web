677c83f9f6a7e055a70f20aeb768a443462805c0
Marcel DiazGranados Roabyo <marceldgr@gmail.com>
2024-12-10 00:53:58 -0500

    Navbar

--- hackintosh/src/App.jsx
+++ hackintosh/src/App.jsx
@@ -1,26 +1,20 @@
+import React from 'react'
 
-import styles from'./App.module.css';
-import{ Navbar} from './Componets/Navbar/Navbar';
-
+import styles from './App.module.css'
+import { Navbar } from './Componets/Navbar/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  
+ 
 
   return (
-    <Router>
-      <div className={styles.App}>
-        <Navbar/>
-        <Routes>
-          <Route path ="/" element={
-            <>
-            </>
-          }/>
-
-        </Routes>
-      </div>
-    </Router>
-  );
+    <>
+    <div className={styles.App}>
+       <Navbar/> 
+    </div>
+    
+    </>
+  )
 }
 
 export default App
--- hackintosh/src/Componets/Navbar/Navbar.jsx
+++ hackintosh/src/Componets/Navbar/Navbar.jsx
@@ -1,21 +1,51 @@
-import React, { useState } from 'react';
-
+import React,{useState} from "react";
+import { Link, useLocation } from 'react-router-dom';
 import styles from "./Navbar.module.css";
+import {getImageUrl} from "../../utils";
 
-import {getImageUrl }from "../../utils";
+export const Navbar=()=>{
+    const [menuOpen,setmenuOpen]=useState(false);
+    
+    const isHomePage = location.pathname === '/';
+    const handleMenuToggle = () => setMenuOpen(!menuOpen);
 
-export const Navbar = () => {
-    return( 
-        
+    return(
         <nav className={styles.navbar}>
-            <img className={styles.logo}src={getImageUrl("nav/logo_1.png")} alt="logo" />
-            <a className={styles.title} href="/">Hackistosh</a>
-            <div className={styles.menu}>
-                <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-btn" />
+            <div className={styles.logoContainer}>
+                <img 
+                    className={styles.logo} 
+                    src={getImageUrl("nav/logo_1.png")} 
+                    alt="Logo Hackintosh" 
+                />
+                {isHomePage ? (
+                    <a className={styles.title} href="/">Hackintosh</a>
+                ) : (
+                    <Link to="/" className={styles.title}>Hackintosh</Link>
+                )}
             </div>
+         
+            
+            <div className={styles.menu}>
+                <img className={styles.menuBtn} src={
+                    menuOpen 
+                    ? getImageUrl("nav/closeIcon.png")
+                    : getImageUrl("nav/menuIcon.png")
+                }
+                alt="menu-button" onClick={()=>setmenuOpen(!menuOpen)}
+                    
+                    />
+                <ul className={`${styles.menuItem} ${menuOpen && styles.menuOpen}`}
+                onClick={()=>setmenuOpen(false)}
+                >
 
+
+                    <li><a href="https://dortania.github.io/OpenCore-Install-Guide/">Guia dortania</a></li>
+                    <li><a href="#MacOS">Versiones de macOS</a></li>
+                    <li><a href="#Herramientas">Herramientas</a></li>
+                    <li><a href="#Equipos">Equipos</a></li>
+                    <li><a href="#Tienda">Tienda</a></li>
+                </ul>
+            </div>
         </nav>
     )
-    
-
 }
